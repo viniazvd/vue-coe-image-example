c9106972c0ad612688f10e118893bcb967e04189
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-15 13:40:14 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="#app">
+    <vue-coe-image
+      :src="src"
+      fallback="https://cdn-images-1.medium.com/max/1600/1*xjGrvQSXvj72W4zD6IWzfg.jpeg"
+    />
+    <button @click="changeImage">coe</button>
+  </div>
+</template>
+
+<script>
+import VueCoeImage from 'vue-coe-image'
+
+export default {
+  components: { VueCoeImage },
+
+  data () {
+    return {
+      src: 'https://3.bp.blogspot.com/-PRG407gZ9bE/V0TCSHFQKcI/AAAAAAAADjE/KbkLmxIXcjMcx4hKTFnDSQxcdPqGuNNWwCLcB/s1600/flamengologo.png'
+    }
+  },
+
+  methods: {
+    changeImage () {
+      this.src = 'https://www.urbanarts.com.br/imagens/produtos/065033/Detalhes/urubu-do-mengao.jpg'
+    }
+  }
+}
+</script>
