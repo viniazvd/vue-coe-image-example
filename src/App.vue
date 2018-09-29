46e2f82c99db836e0178a54e4e3b2f6b982b08fb
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-29 01:24:24 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,37 @@
     <vue-coe-image
       class="coe"
       :src="src"
-      fallback="https://cdn-images-1.medium.com/max/1600/1*xjGrvQSXvj72W4zD6IWzfg.jpeg"
+      fallback="https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916"
+    />
+
+    <oee
+      class="coe2"
+      src="https://img.freepik.com/free-photo/man-jumping-over-impossible-or-possible-over-cliff-on-sunset-background-business-concept-idea_1323-266.jpg?size=338&ext=jpg"
+      placeholder="https://vignette.wikia.nocookie.net/undertale-rho/images/5/5f/Placeholder.jpg/revision/latest?cb=20180213155916"
     />
     <button class="coe" @click="changeImage">coe</button>
   </div>
 </template>
 
 <script>
-import VueCoeImage from 'vue-coe-image'
-// import VueCoeImage from './lib'
+// import VueCoeImage from 'vue-coe-image'
+import VueCoeImage from './lib'
+import oee from './lib/oee.vue'
 
 export default {
-  components: { VueCoeImage },
+  components: { VueCoeImage, oee },
 
   data () {
     return {
-      src: 'https://i.ytimg.com/vi/Yt9t9e9gmmw/maxresdefault.jpg'
+      src: 'https://image.shutterstock.com/image-photo/business-success-leadership-achievement-people-260nw-411972601.jpg'
     }
   },
+
+  // mounted () {
+  //   setTimeout(() => {
+  //     this.src = 'https://pbs.twimg.com/profile_images/1017516299143041024/fLFdcGsl_400x400.jpg'
+  //   }, 2000)
+  // },
 
   methods: {
     changeImage () {
@@ -41,5 +54,13 @@ export default {
   bottom: 10px;
   max-width: 200px;
   max-height: 200px;
+}
+
+.coe2 {
+  position: absolute;
+  bottom: 10px;
+  max-width: 200px;
+  max-height: 200px;
+  right: 20px;
 }
 </style>
